f8cbb3133e88815e0345f12fb52cbbe9960c9fab
Gnanapriya <gnanapriya.cr@gmail.com>
2022-11-15 22:16:31 +0530

    Initial Commit

---
+++ SwiftUI Starter/SwiftUI_StarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_StarterApp.swift
+//  SwiftUI Starter
+//
+//  Created by gnanapriya.chandran on 15/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_StarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
