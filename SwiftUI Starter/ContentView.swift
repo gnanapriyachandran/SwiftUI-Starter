0570490ec703ab0bf3b174df092f28bd1ae8de86
Gnanapriya <gnanapriya.cr@gmail.com>
2022-11-24 22:05:28 +0530

    First Commit. Download image from web and display with animation and placeholder

--- SwiftUI Starter/ContentView.swift
+++ SwiftUI Starter/ContentView.swift
@@ -7,15 +7,75 @@
 
 import SwiftUI
 
+extension Image {
+    func imageModifier() -> some View {
+        self
+            .resizable()
+            .scaledToFit()
+    }
+    
+    func iconModifer() -> some View {
+        self
+            .imageModifier()
+            .frame(width: 128)
+            .foregroundColor(.purple)
+            .opacity(0.5)
+    }
+    
+}
+
 struct ContentView: View {
+    private let imageURL: String = "https://credo.academy/credo-academy@3x.png"
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+    // MARK: - BASIC
+       // AsyncImage(url: URL(string: imageURL))
+        
+        // MARK: - SCALE
+        //AsyncImage(url: URL(string: imageURL), scale: 3.0)
+        
+        //MARK: - PLACEHOLDER
+//
+//        AsyncImage(url: URL(string: imageURL),scale: 3.0) { image in
+//            image.imageModifier()
+//        } placeholder: {
+//            Image(systemName: "photo.circle.fill").iconModifer()
+//        }
+//        .padding(40)
+        
+        
+        //MARK: - phase
+//
+//        AsyncImage(url: URL(string: imageURL)) { phase in
+//            //SUCCESS: image loaded
+//            //FAILURE: image failed to load
+//            //EMPTY: No image loaded
+//
+//            if let image = phase.image {
+//                image.imageModifier()
+//            } else if phase.error != nil {
+//                Image(systemName: "ant.circle.fill").iconModifer()
+//            } else {
+//                Image(systemName: "photo.circle.fill").iconModifer()
+//            }
+//
+//        }.padding(40)
+        
+        //MARK: - Animation
+        AsyncImage(url: URL(string: imageURL), transaction: Transaction(animation: .spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0.25))) { phase in
+            switch phase {
+            case .success(let image):
+                image.imageModifier()
+                    .transition(.slide)
+            case .failure(_):
+                Image(systemName: "ant.circle.fill").iconModifer()
+            case .empty:
+                Image(systemName: "photo.circle.fill").iconModifer()
+            @unknown default:
+                ProgressView()
+            }
+       
+        }.padding(40)
+        
     }
 }
 
